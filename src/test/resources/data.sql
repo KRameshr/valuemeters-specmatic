14b72fe1bd8a1013b2a4dac3e27ba78c62ba6d8d
Kuruba Ramesh <115610243+KRameshr@users.noreply.github.com>
2026-06-18 06:30:31 +0530

    Delete src/test/resources directory

--- src/test/resources/data.sql
+++
@@ -1,9 +0,0 @@
-DELETE FROM accounts;
-DELETE FROM users;
-
-INSERT INTO users (id, email, password, name)
-VALUES (1, 'test@example.com', '$2a$10$abc...', 'Test User');
-
--- IMPORTANT: match Specmatic account IDs
-INSERT INTO accounts (id, balance, user_id)
-VALUES (106, 1000.0, 1);
